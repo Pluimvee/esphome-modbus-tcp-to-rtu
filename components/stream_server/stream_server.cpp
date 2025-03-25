dd326312b66194f41dbfef433af1fda62b66a647
Pluimvee <124380379+Pluimvee@users.noreply.github.com>
2025-03-25 12:02:29 +0100

    .

--- components/stream_server/stream_server.cpp
+++ components/stream_server/stream_server.cpp
@@ -218,7 +218,6 @@ void StreamServerComponent::exchange()
 StreamServerComponent::Client::Client(std::unique_ptr<esphome::socket::Socket> socket, std::string identifier)
     : socket(std::move(socket)), identifier{identifier} 
 {
-    last_uart_time = esphome::millis(); // give it a fresh start
     uart_user_ = false;
     disconnected = false;
 }
